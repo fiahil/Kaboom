b9a62ef4562a282e0590eb84cab5fb7430b30211
Fiahil <Fiahil@gmail.com>
2012-12-11 18:47:34 +0100

    Add base solution

---
+++ Kaboom/Sources/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Kaboom
+{
+    [Activity(Label = "Kaboom", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Microsoft.Xna.Framework.AndroidGameActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            MainGame.Activity = this;
+
+            var g = new MainGame();
+            SetContentView(g.Window);
+
+            g.Run();
+        }
+    }
+}
+
---
+++ Kaboom/Sources/MainGame.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Input.Touch;
+
+namespace Kaboom
+{
+    class MainGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public MainGame()
+        {
+            this.graphics = new GraphicsDeviceManager(this);
+            this.graphics.IsFullScreen = true;
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            this.spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.LightBlue);
+            base.Draw(gameTime);
+        }
+    }
+}
