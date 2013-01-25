8583bf0f80a4259e9e600c6aba9122c8e618cd08
Fiahil <fiahil@gmail.com>
2013-01-25 16:20:45 +0100

    Resharped MainActivity

--- Kaboom/Sources/MainActivity.cs
+++ Kaboom/Sources/MainActivity.cs
@@ -6,17 +6,18 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.Xna.Framework;
 
 namespace Kaboom.Sources
 {
     [Activity(Label = "Kaboom", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Microsoft.Xna.Framework.AndroidGameActivity
+    public class MainActivity : AndroidGameActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            MainGame.Activity = this;
+            Game.Activity = this;
 
             var g = new MainGame();
             SetContentView(g.Window);
