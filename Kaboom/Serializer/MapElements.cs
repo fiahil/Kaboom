5e41c1e70085aa458be3646933be626a5f7125b7
Fiahil <fiahil@gmail.com>
2013-02-24 20:47:06 +0100

    Add WPF project to solution

---
+++ Kaboom/Serializer/MapElements.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Kaboom.Serializer
+{
+    public class EntityProxy
+    {
+        public int ZIndex;
+        public string TileIdentifier;
+        public int[] TileFramePerAnim;
+        public int TileFrameSpeed;
+        public int TileTotalAnim;
+    }
+
+    public class BlockProxy : EntityProxy
+    {
+        public bool Detroyable;
+    }
+
+    public class BombProxy : EntityProxy
+    {
+        public int Type;
+    }
+
+    public class SquareProxy
+    {
+        public List<EntityProxy> Entities;
+
+        public SquareProxy()
+        {
+            this.Entities = new List<EntityProxy>();
+        }
+    }
+
+    [XmlInclude(typeof(BlockProxy)), XmlInclude(typeof(BombProxy))]
+    public class MapElements
+    {
+        public SquareProxy[][] Board;
+        public int DimensionX;
+        public int DimensionY;
+
+        /// <summary>
+        /// Initialize a new MapElement by default
+        /// </summary>
+        public MapElements()
+        {
+            this.DimensionX = 0;
+            this.DimensionY = 0;
+            this.Board = null;
+        }
+
+        /// <summary>
+        /// Initialize a new MapElement
+        /// </summary>
+        /// <param name="x">X size of the map (in square)</param>
+        /// <param name="y">Y size of the map (in square)</param>
+        public MapElements(int x, int y)
+        {
+            this.DimensionX = x;
+            this.DimensionY = y;
+            this.Board = new SquareProxy[x][];
+
+            for (var i = 0; i < x; i++)
+            {
+                this.Board[i] = new SquareProxy[y];
+                for (var j = 0; j < y; j++)
+                {
+                    this.Board[i][j] = new SquareProxy();
+                }
+            }
+        }
+    }
+}
---
+++ KaboomEditor/Pages/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Windows;
+using System.Xml.Serialization;
+using Kaboom.Serializer;
+
+namespace KaboomEditor.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var myObject = new MapElements(2, 5);
+
+            myObject.Board[0][0].Entities.Add(new BlockProxy()
+                {
+                    Detroyable = true,
+                    TileFramePerAnim = new[] {1},
+                    TileFrameSpeed = 1,
+                    TileIdentifier = "koukou",
+                    TileTotalAnim = 1,
+                    ZIndex = 0
+                });
+            
+            var mySerializer = new XmlSerializer(typeof(MapElements));
+            // To write to a file, create a StreamWriter object.
+
+            using (var writer = new StreamWriter("myFileName.xml"))
+            {
+                mySerializer.Serialize(writer, myObject);
+            }
+        }
+    }
+}
