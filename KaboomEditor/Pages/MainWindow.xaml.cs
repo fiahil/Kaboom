897c5cb2e82f14cd742b70adb602da38b8498aa0
Fiahil <fiahil@gmail.com>
2013-02-27 13:57:48 +0100

    Board drawing & serialization

--- KaboomEditor/Pages/MainWindow.xaml.cs
+++ KaboomEditor/Pages/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 using System.Xml.Serialization;
 using Kaboom.Serializer;
 
@@ -11,11 +13,39 @@ namespace KaboomEditor.Pages
     /// </summary>
     public partial class MainWindow
     {
+        private MapElements mapElements_;
+
         public MainWindow()
         {
             InitializeComponent();
+            CreateBoard(5, 10);
         }
 
+        private void CreateBoard(int w, int h)
+        {
+            this.mapElements_ = new MapElements(w, h);
+            var uniformGrid = (UniformGrid)this.FindName("Board");
+            if (uniformGrid != null)
+            {
+                uniformGrid.Children.Clear();
+                uniformGrid.Columns = h;
+                uniformGrid.Rows = w;
+                for (var i = 0; i < w; i++)
+                {
+                    for (var j = 0; j < h; j++)
+                    {
+                        uniformGrid.Children.Add(new Label
+                            {
+                                Background = j % 2 == i % 2 ? Brushes.SlateGray : Brushes.Silver,
+                                ToolTip = "(" + i + ", " + j + ")"
+                            });
+                    }
+                }
+            }
+        }
+
+
+        /*
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             var r = new Random(777);
@@ -80,6 +110,35 @@ namespace KaboomEditor.Pages
             using (var writer = new StreamWriter("level1.xml"))
             {
                 mySerializer.Serialize(writer, me);
+            }
+        }
+        */
+
+        private void ButtonSave_OnClick(object sender, RoutedEventArgs e)
+        {
+            var mySerializer = new XmlSerializer(typeof(MapElements));
+
+            var box = (TextBox) this.FindName("TextBoxFilename");
+            if (box != null)
+            {
+                using (var writer = new StreamWriter(box.Text + ".xml"))
+                {
+                    mySerializer.Serialize(writer, this.mapElements_);
+                }
+            }
+        }
+
+        private void ButtonNew_OnClick(object sender, RoutedEventArgs e)
+        {
+            var height = (TextBox) this.FindName("TextBoxHeight");
+            var width = (TextBox) this.FindName("TextBoxWidth");
+
+            if (width != null && height != null)
+            {
+                int h, w;
+                int.TryParse(width.Text, out h);
+                int.TryParse(height.Text, out w);
+                this.CreateBoard(w, h);
             }
         }
     }
